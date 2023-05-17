a077a080958574d4f01de9d56a913636a4ea2e04
Joseph Riddle <joeriddles10@gmail.com>
2023-05-17 08:56:51 -0700

    Add exclude_done

--- find_todos.py
+++ find_todos.py
@@ -17,18 +17,25 @@ def save_todos(
 ):
     with open(path, "w", encoding="utf-8") as todo_file:
         todo_file.write("# TODOs\n")
-        todo_file.write("## Pending\n")
-        [todo_file.write(f"- [ ] {todo}\n") for todo in pending_todos]
-        todo_file.write("\n## Completed\n")
-        [todo_file.write(f"- [x] {todo}\n") for todo in completed_todos]    
+        if pending_todos:
+            todo_file.write("## Pending\n")
+            [todo_file.write(f"- [ ] {todo}\n") for todo in pending_todos]
+        if completed_todos:
+            todo_file.write("\n## Completed\n")
+            [todo_file.write(f"- [x] {todo}\n") for todo in completed_todos]    
 
 
-def find_todos(path: str = ".", exclude: Optional[str] = None) -> Tuple[list[str], list[str]]:
+def find_todos(
+    path: str = ".",
+    exclude: Optional[str] = None,
+    exclude_done: bool = False,
+) -> Tuple[list[str], list[str]]:
     """Recursively find all markdown task items for the given path.
     
     Args:
         path: The path to recursively search for markdown file in.
         exclude: A filename to exclude from the markdown file search.
+        exclude_done: Exclude completed TODOs
 
     Returns:
         A tuple consisting of all found pending todo items and all completed todo items.
@@ -45,7 +52,8 @@ def find_todos(path: str = ".", exclude: Optional[str] = None) -> Tuple[list[str
             continue
         pending_todos, completed_todos = parse_todos(markdown_lines)
         all_pending_todos.extend(pending_todos)
-        all_completed_todos.extend(completed_todos)
+        if not exclude_done:
+            all_completed_todos.extend(completed_todos)
     return all_pending_todos, all_completed_todos
 
 
@@ -66,15 +74,14 @@ def parse_todos(lines) -> Tuple[list[str], list[str]]:
 
 
 if __name__ == "__main__":
-    watch = False
-    if sys.argv[-1] == "--watch":
-        watch = True
+    watch = "--watch" in sys.argv
+    exclude_done = "--exclude-done" in sys.argv
 
     if watch:
         while True:
-            todos = find_todos("notes", exclude="TODO.md")
+            todos = find_todos("notes", exclude="TODO.md", exclude_done=exclude_done)
             save_todos(*todos, path="notes/TODO.md")
             time.sleep(1)
     else:
-        todos = find_todos("notes", exclude="TODO.md")
+        todos = find_todos("notes", exclude="TODO.md", exclude_done=exclude_done)
         save_todos(*todos, path="notes/TODO.md")
